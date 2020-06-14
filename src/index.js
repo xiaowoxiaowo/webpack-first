b19a2245ff531a6ddda70733900152dfdf85dde3
xiaowoxiaowo <awlzluggeoo@sina.cn>
2020-06-15 00:12:24 +0800

    webpack学习 1.初始化 2.将js转义为低版本 3.mode 4.在浏览器中查看页面

---
+++ src/index.js
@@ -0,0 +1,16 @@
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+  getName() {
+    return this.name
+  }
+}
+
+const dog = new Animal('dog')
+
+let arr = ['1', '2', '3']
+let newArr = arr.forEach(res => {
+  res = '2'
+})
+console.log(newArr)
